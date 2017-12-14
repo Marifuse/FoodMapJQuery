b6568afadab3b5cc57f851e169e38b25afb522a8
Marifuse <maria.fuenzalida.serrano@gmail.com>
2017-12-14 11:56:54 -0300

    Quinto avance CSS y JS (Función del Mouse y algunos agregados a versión PC)

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$("#Chilean-food").mouseover(function(){
+    		alert("¿Quieres restaurants de comida chilena?");
+  	});
+  	$("#Chinese-food").mouseover(function(){
+    		alert("¿Quieres restaurants de comida china?");
+  	});
+  	$("#Indian-food").mouseover(function(){
+    		alert("¿Quieres restaurants de comida hindú?");
+  	});
+  	$("#Italian-food").mouseover(function(){
+    		alert("¿Quieres restaurants de comida italiana?");
+  	});
+  	$("#Japanese-food").mouseover(function(){
+    		alert("¿Quieres restaurants de comida japonesa?");
+  	});
+  	$("#Mexican-food").mouseover(function(){
+    		alert("¿Quieres restaurants de comida mexicana?");
+  	});
+});
+
