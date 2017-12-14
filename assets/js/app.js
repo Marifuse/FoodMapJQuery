c8c91ec7954a6eb113e59cabb2e237849aba6732
Marifuse <maria.fuenzalida.serrano@gmail.com>
2017-12-14 12:19:16 -0300

    Sexto avance JS y algunos agregados HTML (Agregando función mouse a versión PC)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	//Evento de Mouse para versión movil
 	$("#Chilean-food").mouseover(function(){
     		alert("¿Quieres restaurants de comida chilena?");
   	});
@@ -17,5 +18,25 @@ $(document).ready(function(){
   	$("#Mexican-food").mouseover(function(){
     		alert("¿Quieres restaurants de comida mexicana?");
   	});
+  	//Evento de Mouse para Versión PC
+  	$("#Chilean-foodPC").mouseover(function(){
+    		alert("¿Quieres restaurants de comida chilena?");
+  	});
+  	$("#Chinese-foodPC").mouseover(function(){
+    		alert("¿Quieres restaurants de comida china?");
+  	});
+  	$("#Indian-foodPC").mouseover(function(){
+    		alert("¿Quieres restaurants de comida hindú?");
+  	});
+  	$("#Italian-foodPC").mouseover(function(){
+    		alert("¿Quieres restaurants de comida italiana?");
+  	});
+  	$("#Japanese-foodPC").mouseover(function(){
+    		alert("¿Quieres restaurants de comida japonesa?");
+  	});
+  	$("#Mexican-foodPC").mouseover(function(){
+    		alert("¿Quieres restaurants de comida mexicana?");
+  	});
+
 });
 
